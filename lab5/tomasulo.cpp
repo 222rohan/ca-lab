fcf2e9a80bd7fdfda0aa0840776bacd4079a36b2
222rohan <rohanshenoy405@gmail.com>
2025-03-15 12:04:12 +0530

    finish scheduling, (need to fix dependency check)
    Co-authored-by: Dharun Thota <DharunThota@users.noreply.github.com>

--- lab5/tomasulo.cpp
+++ lab5/tomasulo.cpp
@@ -17,13 +17,15 @@ vector<instruction> instructions;
 
 typedef struct {
     int instr_index;
-    int time;
+    int time_to_finish;
     int busy;
+    int idle; //instruction is ready to be executed, but waiting for result
     string op;
     string Vj;
     string Vk;
     string Qj;
     string Qk;
+
 } reservation_station;
 map<string, vector<reservation_station>> reservation_stations;
 
@@ -38,12 +40,12 @@ map<string, int> functional_units = {
     {"LU", 0}
 };
 map<string, int> exec_time = {
-    {"ADD", 6},
-    {"MUL", 12},
+    {"ADD", 2},
+    {"MUL", 10},
     {"FADD", 18},
-    {"FMUL", 30},
-    {"LD", 1},
-    {"SD", 1},
+    {"FMUL", 40},
+    {"LD", 2},
+    {"SD", 2},
     {"LU", 1}
 };
 
@@ -57,46 +59,100 @@ int is_valid_opcode(string opcode) {
 }
 
 void init_reservation_stations(){
-    reservation_station add1, add2, mul1, mul2, fadd1, fadd2, fadd3, fadd4, fmul1, fmul2, fmul3, fmul4, ld1, ld2, sd1, sd2, lu1, lu2;
+    // reservation_station add1, add2, mul1, mul2, fadd1, fadd2, fadd3, fadd4, fmul1, fmul2, fmul3, fmul4, ld1, ld2, sd1, sd2, lu1, lu2;
+    // vector<reservation_station> temp;
+    // temp.push_back(add1);
+    // temp.push_back(add2);
+    // reservation_stations["ADD"] = temp;
+    // temp.clear();
+
+    // temp.push_back(mul1);
+    // temp.push_back(mul2);
+    // reservation_stations["MUL"] = temp;
+    // temp.clear();
+
+    // temp.push_back(fadd1);
+    // temp.push_back(fadd2);
+    // temp.push_back(fadd3);
+    // temp.push_back(fadd4);
+    // reservation_stations["FADD"] = temp;
+    // temp.clear();
+
+    // temp.push_back(fmul1);
+    // temp.push_back(fmul2);
+    // temp.push_back(fmul3);
+    // temp.push_back(fmul4);
+    // reservation_stations["FMUL"] = temp;
+    // temp.clear();
+
+    // temp.push_back(ld1);
+    // temp.push_back(ld2);
+    // reservation_stations["LD"] = temp;
+    // temp.clear();
+
+    // temp.push_back(sd1);
+    // temp.push_back(sd2);
+    // reservation_stations["SD"] = temp;
+    // temp.clear();
+
+    // temp.push_back(lu1);
+    // temp.push_back(lu2);
+    // reservation_stations["LU"] = temp;
+
+    //initialize all reservation stations, and set their variables
+    reservation_station rs;
+    rs.busy = 0;
+    rs.idle = 0;
+    rs.Vj = "";
+    rs.Vk = "";
+    rs.Qj = "";
+    rs.Qk = "";
+    rs.time_to_finish = 0;
+    rs.instr_index = -1;
+    
     vector<reservation_station> temp;
-    temp.push_back(add1);
-    temp.push_back(add2);
+    
+    temp.push_back(rs);
+    temp.push_back(rs);
     reservation_stations["ADD"] = temp;
     temp.clear();
 
-    temp.push_back(mul1);
-    temp.push_back(mul2);
+    temp.push_back(rs);
+    temp.push_back(rs);
     reservation_stations["MUL"] = temp;
     temp.clear();
 
-    temp.push_back(fadd1);
-    temp.push_back(fadd2);
-    temp.push_back(fadd3);
-    temp.push_back(fadd4);
+    temp.push_back(rs);
+    temp.push_back(rs);
+    temp.push_back(rs);
+    temp.push_back(rs);
     reservation_stations["FADD"] = temp;
     temp.clear();
 
-    temp.push_back(fmul1);
-    temp.push_back(fmul2);
-    temp.push_back(fmul3);
-    temp.push_back(fmul4);
+    temp.push_back(rs);
+    temp.push_back(rs);
+    temp.push_back(rs);
+    temp.push_back(rs);
     reservation_stations["FMUL"] = temp;
     temp.clear();
 
-    temp.push_back(ld1);
-    temp.push_back(ld2);
+    temp.push_back(rs);
+    temp.push_back(rs);
     reservation_stations["LD"] = temp;
     temp.clear();
 
-    temp.push_back(sd1);
-    temp.push_back(sd2);
+    temp.push_back(rs);
+    temp.push_back(rs);
     reservation_stations["SD"] = temp;
     temp.clear();
 
-    temp.push_back(lu1);
-    temp.push_back(lu2);
+    temp.push_back(rs);
+    temp.push_back(rs);
     reservation_stations["LU"] = temp;
-    
+    temp.clear();
+
+
+
     return;
 }
 
@@ -154,52 +210,162 @@ void instruction_parser( ifstream &file ) {
     file.close();
 }
 
-void execute_instruction(instruction& instr, string opcode, int clock_cycle){
+void execute_instruction(reservation_station &station, string opcode){
+    int time_to_finish = exec_time[opcode];
+    station.time_to_finish = clock_cycle + time_to_finish;
     return;
+}
+
+bool program_complete(bool completed[], int num_instr){
+    for(int i = 0; i < num_instr; i++){
+        if(!completed[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+void check_for_completion(bool completed[]) {
+    //check if any instruction has completed execution
+    for(auto station: reservation_stations) {
+        for(auto rs: station.second){
+            if(rs.busy && rs.time_to_finish == clock_cycle){
+                rs.busy = 0;
+                rs.Vj = "";
+                rs.Vk = "";
+                rs.Qj = "";
+                rs.Qk = "";
+                instructions[rs.instr_index].exec_comp = clock_cycle;
+                instructions[rs.instr_index].write_res = clock_cycle + 1;
+                rs.idle = 0;
+                register_result_status[instructions[rs.instr_index].dest] = "";
+                cout << rs.instr_index << "completed" << endl;
+                completed[rs.instr_index] = true;
+
+                cout << "Instruction " << instructions[rs.instr_index].opcode << " " << instructions[rs.instr_index].dest << " " << instructions[rs.instr_index].src1 << " " << instructions[rs.instr_index].src2 << " completed at clock cycle " << clock_cycle << endl;
+            }
+        }
+    }
+
+    //go through all reservation stations, and check for dependencies in result
+    for(auto station: reservation_stations){
+        for(auto rs: station.second){
+            if(rs.busy){
+                if(rs.Qj != "" && register_result_status[rs.Qj] == ""){
+                    rs.Vj = rs.Qj;
+                    rs.Qj = "";
+                }
+                if(rs.Qk != "" && register_result_status[rs.Qk] == ""){
+                    rs.Vk = rs.Qk;
+                    rs.Qk = "";
+                }
+                if(rs.Vj != "" && rs.Vk != ""){
+                    rs.idle = 1;
+                }
+            }
+        }
+    }
+}
+
+void execute_idle_instructions(){
+    for(auto station: reservation_stations){
+        for(auto rs: station.second){
+            if(rs.idle && rs.Vj != "" && rs.Vk != ""){
+                execute_instruction(rs, rs.op);
+            }
+        }
+    }
 }
 
 void schedule_instructions(){
     //inorder issue
     int num_instr = instructions.size();
-    for(auto instr: instructions){
-        instr.issue = clock_cycle;
-        string opcode = instr.opcode;
+    bool completed[num_instr];
+    int issued_instruction = 0;
+    for(int i = 0; i < num_instr; i++){
+        completed[i] = false;
+    }
+    
+    while(!program_complete(completed, num_instr) && clock_cycle < 100){
 
-        if(opcode == "AND" || opcode == "OR" || opcode == "XOR" || opcode == "NOT"){
-            opcode = "LU";
+        cout << "Clock cycle: " << clock_cycle << endl;
+        
+        for(auto i: completed){
+            cout << i << " ";
         }
+        cout << endl;
 
-        for(auto station: reservation_stations[opcode]){
-            if(!station.busy){
-                station.busy = 1;
-                station.op = instr.opcode;
-                string station_name = opcode + to_string(station.instr_index + 1);
-                register_result_status[instr.dest] = station_name;
 
-                if(register_result_status[instr.src1] != ""){
-                    station.Qj = register_result_status[instr.src1];
-                }
-                else{
-                    station.Vj = instr.src1;
-                }
+        //check for reservation stations with instructions ready to be executed (no dependencies)
+        execute_idle_instructions();
 
-                if(register_result_status[instr.src2] != ""){
-                    station.Qk = register_result_status[instr.src2];
-                }
-                else{
-                    station.Vk = instr.src2;
-                }
+        //issue the instruction
+        if(!completed[issued_instruction]){
+            instruction instr = instructions[issued_instruction];
+            string opcode = instr.opcode;
 
-                if(station.Vj != "" && station.Vk != ""){
-                    //execute the instruction
-                    execute_instruction(instr, opcode, clock_cycle);
-                    register_result_status[instr.dest] = "";
-                }
-
+            if(opcode == "AND" || opcode == "OR" || opcode == "XOR" || opcode == "NOT"){
+                opcode = "LU";
             }
+
+            for(auto &station: reservation_stations[opcode]){
+                if(!station.busy) {
+                    instructions[issued_instruction].issue = clock_cycle;
+                    station.busy = 1;
+                    station.idle = 0;
+                    station.op = instr.opcode;
+                    register_result_status[instr.dest] = instr.dest;
+
+                    //check if the source registers are ready
+                    cout << "Checking for dependencies" << endl;
+                   // cout << "Register result status: " << register_result_status[instr.src1] << " " << register_result_status[instr.src2] << endl;
+                    
+                   //register result status is empty for all cases for wahteevr reason pls fix
+                   //also the Qj and Qk are not being set properly
+                   //something to do with map maybe?
+
+                    if(register_result_status[instr.src1] != ""){
+                        station.Qj = register_result_status[instr.src1];
+                    }
+                    else{
+                        station.Vj = instr.src1;
+                    }
+
+                    if(register_result_status[instr.src2] != ""){
+                        station.Qk = register_result_status[instr.src2];
+                    }
+                    else{
+                        station.Vk = instr.src2;
+                    }
+
+                    if(station.Vj != "" && station.Vk != ""){
+                        //execute the instruction
+                        cout << "vj" << station.Vj << " vk" << station.Vk <<  " qj" << station.Qj << " qk" << station.Qk << endl;
+                        cout << "Executing instruction: " << instr.opcode << " " << instr.dest << " " << instr.src1 << " " << instr.src2 << endl;
+                        execute_instruction(station, opcode);
+                        register_result_status[instr.dest] = "";
+                    }
+
+                    station.instr_index = issued_instruction;
+                    issued_instruction++;
+                    break;
+                }
+            }
+
         }
+
+        //check if any instruction has completed execution
+        check_for_completion(completed);
 
         clock_cycle++;
+
+    }
+}
+
+void print_stats(){
+    cout << "Instruction\tIssue\tExec\tWrite Res" << endl;
+    for(auto instr: instructions){
+        cout << instr.opcode << " " << instr.dest << " " << instr.src1 << " " << instr.src2 << "\t" << instr.issue << "\t" << instr.exec_comp << "\t" << instr.write_res << endl;
     }
 }
 
@@ -216,6 +382,8 @@ int main() {
     init_reservation_stations();
 
     schedule_instructions();
+
+    print_stats();
 
     return 0;
 }
